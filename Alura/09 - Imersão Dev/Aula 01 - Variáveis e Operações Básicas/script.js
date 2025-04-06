133f77a67e8771e50ac73c15881ead8381d7900b
Pedro Hilbert Silva Guerra <pedrohilbertsg@gmail.com>
2025-04-06 14:46:36 -0300

    Create script.js

---
+++ Alura/09 - Imersão Dev/Aula 01 - Variáveis e Operações Básicas/script.js
@@ -0,0 +1,20 @@
+// Aula 01 - Primeiros comandos em JavaScript
+
+// Primeira coisa que foi revisado foi como imprimir algo no console. Bem simples
+console.log('Oi')
+
+// Também dá pra mostrar um alerta na tela, tipo uma caixinha de aviso
+alert("Olá Mundo!!!")
+
+// prompt serve pra pedir algo do usuário. Aparece uma caixinha com campo pra digitar
+prompt("Digite um valor em wons")
+
+// Aqui tô criando uma variável. Em JS dá pra fazer isso sem declarar tipo, só colocar o nome e o valor
+umwon = 0.000
+
+// Outro exemplo de prompt, agora guardando a resposta do usuário numa variável chamada idade
+idade = prompt("Quantos anos você tem?")
+
+// Uma coisa importante: JavaScript é case sensitive, então 'idade' e 'Idade' são coisas diferentes
+
+// Outra coisa: dá pra comentar o código com // (linha única) ou /* */ (várias linhas)
